43d2d43a5985c3479baf679ce18a4d9c4ef807f7
ackuser <mail@ackarim.com>
2015-10-13 15:01:41 +0100

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cssradialApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cssradialApp
+ */
+angular.module('cssradialApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cssradialApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cssradialApp
+ */
+angular.module('cssradialApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
