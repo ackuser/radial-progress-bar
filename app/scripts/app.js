2c8fcdc0f1447d17fcf789d71fe9c06541d2c56c
ackuser <mail@ackarim.com>
2015-10-13 17:05:35 +0100

    Adding svg examples

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'angular-svg-round-progress'
   ])
   .config(function ($routeProvider) {
     $routeProvider
